43faae90945d0765c97360f1c192b0acb98b699b
Anisse Sadri <anisse.s@icloud.com>
2022-11-22 03:02:42 +0100

    amélioration

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -1,18 +1,20 @@
 const passwordValidator = require("password-validator");
 const schema = new passwordValidator();
+const schema2 = new passwordValidator();
 
-schema
+schema.is().min(5).is().max(100);
+schema2
   .has()
   .not()
   .spaces()
   .has(/[a-zA-Z][@][a-zA-Z]+[.][a-zA-Z]/g);
 
 module.exports = (req, res, next) => {
-  if (schema.validate(req.body.email)) {
+  if (schema2.validate(req.body.email) && schema.validate(req.body.password)) {
     next();
   } else {
     res.status(400).json({
-      error: "L'adresse email n'est pas valide",
+      error: "L'adresse email ou le mot de passe n'est pas assez fort",
     });
   }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const passwordValidator = require("../middleware/password");
-const emailValidator = require("../middleware/email");
+const validator = require("../middleware/validator");
 
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", emailValidator, passwordValidator, userCtrl.signup);
+router.post("/signup", validator, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
